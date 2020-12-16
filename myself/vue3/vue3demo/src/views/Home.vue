16681f5cc5a2d40db2712228962f01e58fef4bd4
flygg123 <s8752215@126.com>
2020-12-16 17:48:17 +0800

    文件上传

--- myself/vue3/vue3demo/src/views/Home.vue
+++ myself/vue3/vue3demo/src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button @click="confirm">按钮</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
@@ -15,5 +16,12 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
   },
 })
 export default class Home extends Vue {
+  private msg: string = "sdiafjla";
+  private created() {
+    console.log(this.msg);
+  }
+  private confirm() {
+    console.log(1111);
+  }
 }
 </script>
