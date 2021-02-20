490dfc885192f1ccb8d605fdd1fec7834f07cb7c
sushengpeng <s8752215@126.com>
2021-02-20 21:34:00 +0800

    java学习

---
+++ myself/java/united1/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    //类体中不允许直接编写java语句除了声明变量除外
+    //static表示静态 void表示空 public表示公开
+    static int i = 1111;
+    public static void main(String[] args) {//表示公开一个静态的主方法
+        // write your code here
+        System.out.println("java的第一个程序");
+        System.out.println(i);
+    }
+}
